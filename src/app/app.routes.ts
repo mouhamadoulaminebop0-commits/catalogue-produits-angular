e6febcb3e27c2c41eefc3de52b310f0e8815e778
@BOPMO <mouhamadoubop7@gmail.com>
2026-09-15 23:46:14 +0000

    Integration de la navbar, du footer et du routage complet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
+import { ProductList } from './pages/product-list/product-list';
+import { ProductDetail } from './pages/product-detail/product-detail';
 import { About } from './pages/about/about';
 import { Contact } from './pages/contact/contact';
 
 export const routes: Routes = [
+  { path: '', component: ProductList },
+  { path: 'produits/:id', component: ProductDetail },
   { path: 'about', component: About },
   { path: 'contact', component: Contact },
+  { path: '**', redirectTo: '' }
 ];
--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,17 +1,8 @@
 import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-contact',
-  styleUrl: './contact.css',
-  templateUrl: './contact.html',
-})
-export class Contact {}
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  standalone: true,
   imports: [FormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css'
